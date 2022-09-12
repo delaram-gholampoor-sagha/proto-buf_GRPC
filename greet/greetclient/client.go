d1ba6903e84616124cd51cc12ad2682bcd841579
Delaram-Gholampoor-Sagha <majesticdelaram@yahoo.com>
2022-09-12 13:44:44 +0430

    uncommented client.go

--- greet/greetclient/client.go
+++ greet/greetclient/client.go
@@ -1,21 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/Delaram-Gholampoor-Sagha/proto-buf_GRPC/greet/greetpb"
+	"google.golang.org/grpc"
+)
 
 // protoc --go_out=. --go_opt=paths=source_relative     --go-grpc_out=. --go-grpc_opt=paths=source_relative     greet/greetpb/greet.proto
 
 // https://github.com/techschool/pcbook-go/issues/3
 
-// func main() {
-// 	fmt.Println(" Hello I'm a client")
+func main() {
+	fmt.Println(" Hello I'm a client")
 
-// 	// cc = client connection
-// 	cc ,err :=  grpc.Dial("localhost:50051" , grpc.WithInsecure())
-// 	if err != nil {
-// 		log.Fatal("could not connect %v" , err)
-// 	}
+	// cc = client connection
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("could not connect %v", err)
+	}
 
-// 	defer cc.Close()
-//     c := greetpb.NewGreetServiceClient(cc)
-// 	fmt.Println("Created CLient %f" , c)
+	defer cc.Close()
+	c := greetpb.NewGreetServiceClient(cc)
+	fmt.Println("Created CLient %f", c)
 
-// }
+}
