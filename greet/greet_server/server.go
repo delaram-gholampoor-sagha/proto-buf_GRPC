e6fac5a2571cadf0a97f48b16173b2f5baf1fd30
Delaram-Gholampoor-Sagha <majesticdelaram@yahoo.com>
2022-09-12 13:43:06 +0430

    (Unary) API implementation

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,24 +1,41 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
 
+	"github.com/Delaram-Gholampoor-Sagha/proto-buf_GRPC/greet/greetpb"
+	"google.golang.org/grpc"
+)
 
-// type server struct {
+type server struct {
+	greetpb.UnimplementedGreetServiceServer
+}
 
-// }
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	firstname := req.GetGreeting().GetFirstName()
+	result := "Hello " + firstname
+	res := &greetpb.GreetResponse{
+		Result: result,
+	}
 
+	return res, nil
+}
 
-// func main() {
-// 	fmt.Print("Hello world")
-// 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
-// 	if err != nil {
-// 		log.Fatal("Failed to Listen: %v", err)
-// 	}
+func main() {
+	fmt.Print("Hello world")
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatal("Failed to Listen: %v", err)
+	}
 
-// 	s := grpc.NewServer()
-// 	greetpb.RegisterGreetService(s, &server{})
+	s := grpc.NewServer()
 
+	greetpb.RegisterGreetServiceServer(s, &server{})
 
-// 	if s.Serve(lis); err != nil {
-//           log.Fatalf("failed to serve: %v" , err)
-// 	}
-// }
+	if s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
