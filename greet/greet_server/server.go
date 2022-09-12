9d06947859fab753053710e09826fd0f185e28af
Delaram-Gholampoor-Sagha <majesticdelaram@yahoo.com>
2022-09-12 14:56:10 +0430

    server streaming api server implementation

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strconv"
+	"time"
 
 	"github.com/Delaram-Gholampoor-Sagha/proto-buf_GRPC/greet/greetpb"
 	"google.golang.org/grpc"
@@ -15,7 +17,7 @@ type server struct {
 }
 
 func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
-	fmt.Printf("Greet function was invoked witj %v", req)
+	fmt.Printf("Greet function was invoked with\n %v", req)
 	firstname := req.GetGreeting().GetFirstName()
 	result := "Hello " + firstname
 	res := &greetpb.GreetResponse{
@@ -23,6 +25,21 @@ func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.G
 	}
 
 	return res, nil
+}
+
+func (*server) GreetManyTimes(req *greetpb.GreetManyTimesRequest , stream greetpb.GreetService_GreetMnayTimesServer) error {
+	fmt.Printf("GreetManytimes function was invoked with\n %v", req)
+	firstName := req.GetGreeting().GetFirstName()
+	for i := 0 ; i < 10 ; i++ {
+		result := "Hello " + firstName + "number " + strconv.Itoa(i)
+		res := &greetpb.GreetManyTimesResponse{
+			Result: result,
+		}
+
+		stream.Send(res)
+		time.Sleep(1000 * time.Millisecond)
+	}
+	return nil
 }
 
 func main() {
